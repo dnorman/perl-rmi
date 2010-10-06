96eaf335d07cba7f91ebe3f4d51da7e1907819b1
Daniel <daniel@danielnorman.net>
2010-10-05 18:08:55 -0700

    ripped off some catalyst code, very rough nonworking state

---
+++ lib/RMI/Exception.pm
@@ -0,0 +1,48 @@
+package RMI::Exception;
+
+use Moose;
+use Carp;
+
+has message => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => sub { $! || '' },
+);
+
+use overload 
+  '""'     => \&as_string,
+  fallback => 1;
+
+sub as_string {
+    my ($self) = @_;
+    return $self->message;
+}
+
+around BUILDARGS => sub {
+
+    my ($next, $class, @args) = @_;
+    if (@args == 1 && !ref $args[0]) {
+        @args = (message => $args[0]);
+    }
+
+    my $args = $class->$next(@args);
+    $args->{message} ||= $args->{error} if exists $args->{error};
+
+    return $args;
+};
+
+sub throw {
+    my $class = shift;
+    my $error = $class->new(@_);
+    local $Carp::CarpLevel = 1;
+    croak $error;
+}
+
+sub rethrow {
+    my ($self) = @_;
+    croak $self;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
