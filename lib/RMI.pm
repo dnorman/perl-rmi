ea7afbc63d37ed0e8bedc9d214af2a84ded887f7
Daniel <daniel@danielnorman.net>
2010-10-05 18:10:41 -0700

    ripped off some catalyst code, very rough nonworking state

---
+++ lib/RMI.pm
@@ -0,0 +1,108 @@
+package RMI;
+
+use Module::Pluggable::Object;
+use RMI::Exception;
+use Class::MOP;
+
+sub setup {
+    my ( $class, %param ) = @_;
+
+    $class->setup_engine(  delete $param{engine} );
+    $class->setup_class(   delete $param{engine} );
+}
+
+sub setup_engine {
+    my ( $class, $engine ) = @_;
+
+    $engine = 'RMI::Engine::' . $engine if $engine;
+
+    if ( $ENV{MOD_PERL} ) {
+	  #my $meta = Class::MOP::get_metaclass_by_name($class);
+	  # create the apache method
+	  #$meta->add_method('apache' => sub { shift->engine->apache });
+
+	  my ( $software, $version ) = $ENV{MOD_PERL} =~ /^(\S+)\/(\d+(?:[\.\_]\d+)+)/;
+
+	  $version =~ s/_//g;
+	  $version =~ s/(\.[^.]+)\./$1/g;
+
+	  if ( $software eq 'mod_perl' ) {
+		if ( !$engine ) {
+		      if     ( $version >= 1.99922 ) { $engine = 'RMI::Engine::Apache2'
+		      }elsif ( $version >= 1.9901  ) { $engine = 'RMI::Engine::Apache2'
+		      }elsif ( $version >= 1.24    ) { $engine = 'RMI::Engine::Apache'
+		      }else {
+			    RMI::Exception->throw( "Unsupported mod_perl version: $ENV{MOD_PERL}" );
+		      }
+
+		}
+
+		# install the correct mod_perl handler
+		if ( $version >= 1.9901 ) {
+		      *handler = sub : method { shift->handle_request(@_) };
+		}else {
+		      *handler = sub ($$) { shift->handle_request(@_) };
+		}
+
+	  }else {
+		RMI::Exception->throw( "Unsupported mod_perl: $ENV{MOD_PERL}" );
+	  }
+    }
+
+    Class::MOP::load_class($engine);
+
+    # engine instance
+    $class->engine( $engine->new );
+}
+
+
+sub locate_components {
+    my $class  = shift;
+    my $config = shift;
+
+    my @paths   = qw( ::Controller ::C ::Model ::M ::View ::V );
+    my $extra   = delete $config->{ search_extra } || [];
+
+    push @paths, @$extra;
+
+    my $locator = Module::Pluggable::Object->new(
+        search_path => [ map { s/^(?=::)/$class/; $_; } @paths ],
+        %$config
+    );
+
+    my @comps = $locator->plugins;
+
+    return @comps;
+}
+
+sub setup_components {
+    my $class = shift;
+
+    my $config  = $class->config->{ setup_components };
+
+    my @comps = sort { length $a <=> length $b }
+                $class->locate_components($config);
+    my %comps = map { $_ => 1 } @comps;
+
+
+    for my $component ( @comps ) {
+        # We pass ignore_loaded here so that overlay files for (e.g.)
+        # Model::DBI::Schema sub-classes are loaded - if it's in @comps
+        # we know M::P::O found a file on disk so this is safe
+
+        Catalyst::Utils::ensure_class_loaded( $component, { ignore_loaded => 1 } );
+
+        # Needs to be done as soon as the component is loaded, as loading a sub-component
+        # (next time round the loop) can cause us to get the wrong metaclass..
+        $class->_controller_init_base_classes($component);
+    }
+
+    for my $component (@comps) {
+        $class->components->{ $component } = $class->setup_component($component);
+        for my $component ($class->expand_component_module( $component, $config )) {
+            next if $comps{$component};
+            $class->_controller_init_base_classes($component); # Also cover inner packages
+            $class->components->{ $component } = $class->setup_component($component);
+        }
+    }
+}
